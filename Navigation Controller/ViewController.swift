c5e13b17178bbcbe887bf8724c1e09a5143be2e6
Thomas Jurczyk <thomasjurczyk@outlook.com>
2019-09-15 15:13:54 -0500

    Uploaded Files

---
+++ Navigation Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation Controller
+//
+//  Created by Thomas Jurczyk on 4/2/19.
+//  Copyright © 2019 Thomas Jurczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
